54afd8115147582f719371ca45c11f0fead11d11
jeraldjjohnson <jerald@Jeralds-MBP.westell.com>
2016-07-09 17:07:40 -0700

    Updated site

--- tiarra/assets/js/functions.js
+++ tiarra/assets/js/functions.js
@@ -1,4 +1,9 @@
-$(function NavClick(){
+$(function (){
+  navClick();
+  sliderReveal();
+});
+
+function navClick() {
   var $navClick = $(".nav-toggle")
         $pageWrap = $(".page-wrap")
         $sideBar = $(".sidebar");
@@ -14,4 +19,36 @@ $(function NavClick(){
     }
 
   })
-});
+}
+
+function sliderReveal() {
+  $(".slider-controls a").on("click", function () {
+        var $this = $(this),
+          $heroImg = $(".hero-img"),
+          $heroImgPic = $(".hero-img img")
+          curReveal = $heroImg.find(".reveal"),
+          position = $heroImg.children().index(curReveal),
+          imgNum = $heroImgPic.length;
+
+        if ($this.hasClass("next")) {
+          if ( position < imgNum -1 ) {
+            curReveal.removeClass("reveal");
+            curReveal.next().addClass("reveal");
+          } else {
+            curReveal.removeClass("reveal");
+            $heroImgPic.first().addClass("reveal")
+          }
+        } else {
+          if (position != 0 ) {
+            curReveal.removeClass("reveal");
+            curReveal.prev().addClass("reveal");
+          } else {
+            curReveal.removeClass("reveal");
+            $heroImgPic.last().addClass("reveal");
+          }
+        }
+
+
+  })
+
+}
